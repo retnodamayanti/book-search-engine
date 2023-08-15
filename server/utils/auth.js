71bb2d524818c45581f0fabf86b0915a6d2ae6e4
retnodamayanti <mayahere5@gmail.com>
2023-08-16 01:31:18 +0930

    updated auth.js and server, server running

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,28 +5,30 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  authMiddleware: function ({ req, res, next }) {
-    let token = req.headers.authorization || '';
+  authMiddleware: function ({req}) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
-    if (token.startsWith('Bearer ')) {
-      token = token.slice(7, token.length).trim();
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
     }
 
     if (!token) {
-      return res.status(401).json({ message: 'Unauthorized - Missing token' });
+      return req;
     }
 
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-      next();
-    } catch (error) {
-      console.error('Invalid token:', error.message);
-      return res.status(401).json({ message: 'Unauthorized - Invalid token' });
+    } catch {
+      console.log('Invalid token');
     }
+
+    // return the request
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
+
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
