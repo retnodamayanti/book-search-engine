2c444e614a856b6eb38b5758b5fb9f6a95a8e12a
retnodamayanti <mayahere5@gmail.com>
2023-08-16 04:14:28 +0930

    fixing savedbook

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ApolloProvider } from '@apollo/client';
-import { getMe } from './utils/API'; 
+import { ApolloProvider, useQuery } from '@apollo/client';
+// import { getMe } from './utils/API'; 
 import Auth from './utils/auth'; 
 import { GET_ME } from './utils/queries';
 import SearchBooks from './pages/SearchBooks';
@@ -15,44 +15,52 @@ const client = new ApolloClient({
 });
 
 function App() {
-  const isLoggedIn = Auth.loggedIn();
-
-  if (isLoggedIn) {
-    getMe(Auth.getToken())
-      .then(({ data }) => {
-        client.writeQuery({
-          query: GET_ME,
-          data: { me: data },
-        });
-      })
-      .catch(err => {
-        console.error(err);
-      });
-  }
-
+  console.log("App component rendering..."); 
   return (
     <ApolloProvider client={client}>
       <Router>
         <>
           <Navbar />
           <Routes>
-            <Route 
-              path='/' 
-              element={<SearchBooks />} 
+            <Route path='/' element={<SearchBooks />} />
+            <Route
+              path='/saved'
+              element={
+                <PrivateRoute>
+                  <SavedBooks />
+                </PrivateRoute>
+              }
             />
-            <Route 
-              path='/saved' 
-              element={isLoggedIn ? <SavedBooks /> : <h1>Please log in to view your saved books.</h1>} 
-            />
-            <Route 
-              path='*'
-              element={<h1 className='display-2'>Wrong page!</h1>}
-            />
+            <Route path='*' element={<h1 className='display-2'>Wrong page!</h1>} />
           </Routes>
         </>
       </Router>
     </ApolloProvider>
   );
+}
+
+function PrivateRoute({ children }) {
+  console.log("PrivateRoute component rendering..."); 
+
+  const { loading, data } = useQuery(GET_ME);
+
+  console.log("PrivateRoute data:", data); 
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  const isLoggedIn = Auth.loggedIn();
+  const userData = data?.me;
+
+  console.log("PrivateRoute isLoggedIn:", isLoggedIn); 
+  console.log("PrivateRoute userData:", userData); 
+
+  if (!isLoggedIn || !userData) {
+    return <h1>Please log in to view your saved books.</h1>;
+  }
+
+  return children;
 }
 
 export default App;
